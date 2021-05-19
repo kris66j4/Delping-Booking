0130fd81a8e962c2dc2c6ad0c2a582c5a9e314a0
Kristian Rosenkilde Pedersen <kris66j4@edu.ucl.dk>
2021-05-19 14:51:10 +0200

    randomshit

--- Delpin Booking/Data/DelpinBookingContext.cs
+++ Delpin Booking/Data/DelpinBookingContext.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
 using Delpin_Booking.Models;
 
 namespace Delpin_Booking.Data
 {
-    public class DelpinBookingContext
+    public class DelpinBookingContext : DbContext
     {
-
         public DelpinBookingContext(DbContextOptions<DelpinBookingContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Ressource> Ressources { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
-
