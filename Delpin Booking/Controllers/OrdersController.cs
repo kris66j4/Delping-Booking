60407b4ac10a724167545834a0ddc3329ff7f4d7
Tobias Myrvoll <tobi5116@edu.ucl.dk>
2021-05-26 17:46:11 +0200

    Har færdiggjort CustomerController og OrderController. De kalder nu API. Slettet context forbindelse

--- Delpin Booking/Controllers/OrdersController.cs
+++ Delpin Booking/Controllers/OrdersController.cs
@@ -13,12 +13,9 @@ namespace Delpin_Booking.Controllers
 {
     public class OrdersController : Controller
     {
-        private readonly DelpinBookingContext _context;
-
-        public OrdersController(DelpinBookingContext context)
-        {
-            _context = context;
-        }
+       private IEnumerable<Customer> customer = null;
+       private IEnumerable<Ressource> ressource = null;
+        
 
         // GET: Orders
         public ActionResult Index()
@@ -49,38 +46,82 @@ namespace Delpin_Booking.Controllers
         }
 
 
-        //public async Task<IActionResult> Index()
-        //{
-        //    var delpinBookingContext = _context.Orders.Include(o => o.Customer).Include(o => o.Ressource);
-        //    return View(await delpinBookingContext.ToListAsync());
-        //}
-
         // GET: Orders/Details/5
         public async Task<IActionResult> Details(int? id)
         {
+
             if (id == null)
             {
                 return NotFound();
             }
-
-            var order = await _context.Orders
-                .Include(o => o.Customer)
-                .Include(o => o.Ressource)
-                .FirstOrDefaultAsync(m => m.OrderId == id);
-            if (order == null)
+            Order order = null;
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                client.BaseAddress = new Uri("https://localhost:44362/api/");
+                var responseTask = client.GetAsync($"Orders/{id}");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<Order>();
+                    readJob.Wait();
+                    order = readJob.Result;
+                }
+                else
+                {
+                    //Return the error code here
+                    //customer = EnumerableEmpty<Customer>();
+                    ModelState.AddModelError(string.Empty, "Server fejl rip.");
+                }
+                return View(order);
             }
 
-            return View(order);
+            
         }
 
         // GET: Orders/Create
         public IActionResult Create()
         {
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId");
-            ViewData["RessourceId"] = new SelectList(_context.Ressources, "RessourceId", "RessourceId");
-            return View();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44362/api/");
+
+                var responseTask2 = client.GetAsync($"Customers");
+                var responseTask3 = client.GetAsync($"Ressources");
+                responseTask3.Wait();
+
+
+                var result2 = responseTask2.Result;
+                var result3 = responseTask3.Result;
+                if (result2.IsSuccessStatusCode)
+                {
+                    var readJob2 = result2.Content.ReadAsAsync<IList<Customer>>();
+                    readJob2.Wait();
+                    customer = readJob2.Result;
+                }
+                else
+                {
+                    //Return the error code here
+
+                    ModelState.AddModelError(string.Empty, "Responsetask 2 fejl rip.");
+                }
+                if (result3.IsSuccessStatusCode)
+                {
+                    var readJob3 = result3.Content.ReadAsAsync<IList<Ressource>>();
+                    readJob3.Wait();
+                    ressource = readJob3.Result;
+                }
+                else
+                {
+                    //Return the error code here
+
+                    ModelState.AddModelError(string.Empty, "Responsetask 3 fejl rip.");
+                }
+                ViewData["CustomerId"] = new SelectList(customer, "CustomerId", "CustomerId");
+                ViewData["RessourceId"] = new SelectList(ressource, "RessourceId", "RessourceId");
+                return View();
+            }
         }
 
         // POST: Orders/Create
@@ -88,17 +129,27 @@ namespace Delpin_Booking.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("OrderId,CustomerId,RessourceId,Date,Price")] Order order)
+        public async Task<IActionResult> Create([Bind("OrderId,CustomerId,RessourceId,Date,BookingStart,BookingEnd,Price")] Order order)
         {
-            if (ModelState.IsValid)
+            using (var client = new HttpClient())
             {
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                client.BaseAddress = new Uri("https://localhost:44362/api/");
+                var postJob = client.PostAsJsonAsync<Order>("Orders", order);
+                postJob.Wait();
+                var postResult = postJob.Result;
+                if (postResult.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "yo recked its crashed fool");
+                }
+                
+                return View(order);
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", order.CustomerId);
-            ViewData["RessourceId"] = new SelectList(_context.Ressources, "RessourceId", "RessourceId", order.RessourceId);
-            return View(order);
+ 
         }
 
         // GET: Orders/Edit/5
@@ -108,15 +159,60 @@ namespace Delpin_Booking.Controllers
             {
                 return NotFound();
             }
-
-            var order = await _context.Orders.FindAsync(id);
-            if (order == null)
+           
+            Order order = null;
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                client.BaseAddress = new Uri("https://localhost:44362/api/");
+                var responseTask = client.GetAsync($"Orders/{id}");
+                var responseTask2 = client.GetAsync($"Customers");
+                var responseTask3 = client.GetAsync($"Ressources");
+                responseTask3.Wait();
+
+                var result = responseTask.Result;
+                var result2 = responseTask2.Result;
+                var result3 = responseTask3.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<Order>();
+                    readJob.Wait();
+                    order = readJob.Result;
+                }
+                else
+                {
+                    //Return the error code here
+                    
+                    ModelState.AddModelError(string.Empty, "Responsetask 1 fejl rip.");
+                }
+                if (result2.IsSuccessStatusCode)
+                {
+                    var readJob2 = result2.Content.ReadAsAsync<IList<Customer>>();
+                    readJob2.Wait();
+                    customer = readJob2.Result;
+                }
+                else
+                {
+                    //Return the error code here
+                    
+                    ModelState.AddModelError(string.Empty, "Responsetask 2 fejl rip.");
+                }
+                if (result3.IsSuccessStatusCode)
+                {
+                    var readJob3 = result3.Content.ReadAsAsync<IList<Ressource>>();
+                    readJob3.Wait();
+                    ressource = readJob3.Result;
+                }
+                else
+                {
+                    //Return the error code here
+                    
+                    ModelState.AddModelError(string.Empty, "Responsetask 3 fejl rip.");
+                }
+                ViewData["CustomerId"] = new SelectList(customer, "CustomerId", "CustomerId", order.CustomerId);
+                ViewData["RessourceId"] = new SelectList(ressource, "RessourceId", "RessourceId", order.RessourceId);
+                return View(order);
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", order.CustomerId);
-            ViewData["RessourceId"] = new SelectList(_context.Ressources, "RessourceId", "RessourceId", order.RessourceId);
-            return View(order);
+            
         }
 
         // POST: Orders/Edit/5
@@ -146,29 +242,42 @@ namespace Delpin_Booking.Controllers
                     ModelState.AddModelError(string.Empty, "yo recked its crashed fool");
                 }
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", order.CustomerId);
-            ViewData["RessourceId"] = new SelectList(_context.Ressources, "RessourceId", "RessourceId", order.RessourceId);
+            
             return View(order);
         }
 
         // GET: Orders/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
+
             if (id == null)
             {
                 return NotFound();
             }
-
-            var order = await _context.Orders
-                .Include(o => o.Customer)
-                .Include(o => o.Ressource)
-                .FirstOrDefaultAsync(m => m.OrderId == id);
-            if (order == null)
+            Order order = null;
+            using (var client = new HttpClient())
             {
-                return NotFound();
-            }
+                client.BaseAddress = new Uri("https://localhost:44362/api/");
+                var responseTask = client.GetAsync($"Orders/{id}");
+                responseTask.Wait();
 
-            return View(order);
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<Order>();
+                    readJob.Wait();
+                    order = readJob.Result;
+                }
+                else
+                {
+                    //Return the error code here
+                    //customer = EnumerableEmpty<Customer>();
+                    ModelState.AddModelError(string.Empty, "Server fejl rip.");
+                }
+                return View(order);
+
+            }
+            
         }
 
         // POST: Orders/Delete/5
@@ -176,15 +285,35 @@ namespace Delpin_Booking.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var order = await _context.Orders.FindAsync(id);
-            _context.Orders.Remove(order);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            Order order = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44362/api/");
+                var responseTask = client.DeleteAsync($"Orders/{id}");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<Order>();
+                    readJob.Wait();
+                    order = readJob.Result;
+                }
+                else
+                {
+                    //Return the error code here
+                    //customer = EnumerableEmpty<Customer>();
+                    ModelState.AddModelError(string.Empty, "Server fejl rip.");
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
         }
 
-        private bool OrderExists(int id)
-        {
-            return _context.Orders.Any(e => e.OrderId == id);
-        }
+        //private bool OrderExists(int id)
+        //{
+        //    return _context.Orders.Any(e => e.OrderId == id);
+        //}
     }
 }
