e0ed35480e8f37dd484ddc81d92a89aeedb97421
kris66j4 <64254960+kris66j4@users.noreply.github.com>
2021-05-30 16:24:25 +0200

    Roles working + fixed price

--- Delpin Booking/Startup.cs
+++ Delpin Booking/Startup.cs
@@ -38,6 +38,7 @@ namespace Delpin_Booking
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
